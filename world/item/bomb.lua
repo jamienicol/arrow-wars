c7fd572ee0965e9cef96b0b97daaad7a05e289ff
Jamie Nicol <jamie@thenicols.net>
2013-12-29 19:42:52 +0000

    world: implement bombs
    items which can be picked up by actors. when they are used they appear
    back on the map and a timer counts down. when the countdown reaches zero
    it causes an explosion.

---
+++ world/item/bomb.lua
@@ -0,0 +1,62 @@
+local class = require("middleclass.middleclass")
+local Explosion = require("world.explosion")
+local Item = require("world.item.item")
+local loader = require("love2d-assets-loader.Loader.loader")
+
+local Bomb = class("world.item.Bomb", Item)
+
+function Bomb:initialize()
+   Item.initialize(self)
+end
+
+function Bomb:get_name()
+   return "Bomb"
+end
+
+function Bomb:get_crate_image()
+   return loader.Image["bomb"]
+end
+
+function Bomb:should_automatically_use()
+   return false
+end
+
+function Bomb:use(agent)
+   self._position = agent:get_position()
+
+   self._countdown = 3
+
+   agent._world:add(self)
+end
+
+function Bomb:update(dt)
+   self._countdown = self._countdown - dt
+
+   if self._countdown <= 0 then
+      self._world:add(Explosion:new(self._position))
+
+      self._world:remove(self)
+   end
+end
+
+function Bomb:draw()
+   local image = loader.Image["bomb"]
+   love.graphics.draw(image,
+                      self._position.x, self._position.y,
+                      0,
+                      1, 1,
+                      image:getWidth() / 2,
+                      image:getHeight() / 2)
+
+   local font = loader.Font[22]
+   love.graphics.setFont(font)
+   love.graphics.setColor(255, 255, 255)
+   love.graphics.printf(tostring(math.ceil(self._countdown)),
+                        self._position.x - image:getWidth() / 2,
+                        self._position.y - image:getHeight() / 2,
+                        image:getWidth(),
+                        "center")
+
+end
+
+return Bomb
